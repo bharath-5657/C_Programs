1a6acd22750dd4829148026a9fde80351e3b8820
bharath-5657 <128080957+bharath-5657@users.noreply.github.com>
2023-03-16 23:43:18 +0530

    Create Countsetbits.c

---
+++ Countsetbits.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int CountBits(unsigned int x) {
+  int count=0;
+  while(x) {
+      count += x & 1;
+      x >>= 1;
+  }
+  return count;
+}
+
+int main() 
+{
+  int num=0;
+  printf("Enter number to countbits:");
+  scanf("%d",&num);
+  printf("No of bits set = %d",CountBits(num));
+  return 0;
+}
