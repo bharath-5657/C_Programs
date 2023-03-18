2b5b4f49d353a78b127d551ee7cf6937222d5fc9
bharath-5657 <128080957+bharath-5657@users.noreply.github.com>
2023-03-18 09:14:43 +0530

    computing the parity of  word
    The parity of word is 1 if number of 1's in world is odd, otherwise 0

---
+++ Parity.c
@@ -0,0 +1,36 @@
+//computing the parity of  word
+//The parity of word is 1 if number of 1's in world is odd, otherwise 0;
+
+#include <stdio.h>
+int Parity(unsigned long long x) {
+  int count=0;
+  while(x)
+  {
+      count += (x&1);
+      x>>=1;
+  }
+  if(count%2==0){
+      return 0;
+  }
+  else {
+      return 1;
+  }
+}
+/*Other solution
+int Parity(unsigned long long x) {
+  int count=0;
+  while(x)
+  {
+      count ^= (x&1);
+      x>>=1;
+  }
+  reutrn count;
+  }*/
+int main()
+{
+  unsigned long long num=0;
+  printf("Enter number:");
+  scanf("%lld",&num);
+  printf("Parity of number is = %d",Parity(num));
+  return 0;
+}
